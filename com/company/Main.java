aef776bbb01ac5a4f0982a75b23eb6c0d1ef462b
nntc <vc@nntc.nnov.ru>
2023-04-28 16:33:06 +0300

    work

--- com/company/Main.java
+++ com/company/Main.java
@@ -2,47 +2,54 @@ package com.company;
 import java.io.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import org.json.JSONObject;
-import org.json.simple.JSONObject;
+//import org.json.JSONObject;
+//import org.json.simple.JSONObject;
 
 
 
 
 public class Main {
 
-
         private static Path filePath = Paths.get("C:\\Users\\Casa\\Desktop\\test(1).mif");
 
     public static void main(String[] args) throws FileNotFoundException {
         System.setProperty("file.encoding", "UTF-8");
-        String path = "C:\\Users\\Casa\\Desktop\\1.mif";
+        String pathWindows = "C:\\Users\\Casa\\Desktop\\1.mif";
+        String pathMacOS = "/Users/administrator/IdeaProjects/MifParser1/test (1).mif";
 
-        String з = "C:\\Users\\Casa\\Desktop\\result.json";
+
+        String  resultPath = "C:\\Users\\Casa\\Desktop\\result.json";
         final int LOOKUP = 0;
         final int READ_KEY = 1;
         final int READ_VALUE = 2;
         int state = LOOKUP;
        // Gson g = new Gson();
-        try(FileInputStream fin=new FileInputStream(path))
+        try(FileInputStream fin=new FileInputStream(pathMacOS))
             {
-                System.out.println("DEBUG 1");
+                System.out.println("Parsing of"+pathMacOS);
+                System.out.println("\"test1\" : {");
                 int i;
                 while((i=fin.read()) != -1){
                     switch (state) {
                         case LOOKUP:
                             //isDigit(String.valueOf(i));
-                            if (((i > 'A') && (i < 'z')) || ((isDigit(String.valueOf(i)) == true)))  {
+                            if (((i > 'A') && (i < 'z')))  {
                                 state = READ_KEY;
-                                System.out.print("{\"");
+                                //System.out.print("{\"");
+                                System.out.print("   ");
                                 System.out.print((char) i);
+                            }
+                            else if(((isDigit(String.valueOf(i)) == true))){
+
                             }
                             break;
 
                         case READ_KEY:
                            // isDigit(String.valueOf(i));
-                            if (((i > 'A') && (i < 'z')) || ((isDigit(String.valueOf(i)) == true))) {
+                            if (((i > 'A') && (i < 'z')) ) {
                                 System.out.print((char) i);
-                            } else {
+                            } else if(((isDigit(String.valueOf(i)) == true))){
+                                System.out.println("check");
                                 System.out.print("\", \"");
                                 state = READ_VALUE;
                             }
@@ -56,9 +63,11 @@ public class Main {
                                 System.out.print((char) i);
                             }
                             break;
+                        default:
+                            throw new IllegalStateException("Unexpected value: " + state);
                     }
                 }
-                System.out.println();
+                System.out.println("}");
             }
         catch(IOException ex){
 
