da377f05887e87e342bd6c797f75c648684c56f6
Seqoi <86360694+Seqoi@users.noreply.github.com>
2023-05-02 09:13:13 +0300

    Коментарии + додумать систему ранжировки элементов по умному

--- com/company/Main.java
+++ com/company/Main.java
@@ -2,7 +2,9 @@ package com.company;
 import java.io.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-//import org.json.JSONObject;
+//import org.json.JSONArray;
+//
+//import javax.xml.crypto.Data;
 //import org.json.simple.JSONObject;
 
 
@@ -16,7 +18,8 @@ public class Main {
         System.setProperty("file.encoding", "UTF-8");
         String pathWindows = "C:\\Users\\Casa\\Desktop\\1.mif";
         String pathMacOS = "/Users/administrator/IdeaProjects/MifParser1/test (1).mif";
-
+//        Gson g = new Gson;
+//        Data d = g.fromJson(jsonString, Data.class);
 
         String  resultPath = "C:\\Users\\Casa\\Desktop\\result.json";
         final int LOOKUP = 0;
@@ -24,14 +27,14 @@ public class Main {
         final int READ_VALUE = 2;
         int state = LOOKUP;
        // Gson g = new Gson();
-        try(FileInputStream fin=new FileInputStream(pathMacOS))
+        try(FileInputStream fin=new FileInputStream(pathWindows))
             {
-                System.out.println("Parsing of"+pathMacOS);
+                System.out.println("Parsing of"+pathWindows);
                 System.out.println("\"test1\" : {");
                 int i;
                 while((i=fin.read()) != -1){
                     switch (state) {
-                        case LOOKUP:
+                        case LOOKUP: //Чтение первого символа
                             //isDigit(String.valueOf(i));
                             if (((i > 'A') && (i < 'z')))  {
                                 state = READ_KEY;
@@ -40,22 +43,24 @@ public class Main {
                                 System.out.print((char) i);
                             }
                             else if(((isDigit(String.valueOf(i)) == true))){
+                                System.out.print((char) i);
 
                             }
                             break;
 
-                        case READ_KEY:
+                        case READ_KEY:// чтение последующих символов
                            // isDigit(String.valueOf(i));
                             if (((i > 'A') && (i < 'z')) ) {
                                 System.out.print((char) i);
                             } else if(((isDigit(String.valueOf(i)) == true))){
-                                System.out.println("check");
-                                System.out.print("\", \"");
+                                System.out.print((char) i);
+                                System.out.println("{");
+                                System.out.print("\"");
                                 state = READ_VALUE;
                             }
                             break;
 
-                        case READ_VALUE:
+                        case READ_VALUE:// чтение значения ключа до конца строки
                             if (i == '\n') {
                                 System.out.println("\"}");
                                 state = LOOKUP;
@@ -84,4 +89,5 @@ public class Main {
         }
     }
 }
+
 
